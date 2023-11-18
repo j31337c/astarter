ec15fade507f6e517d6fadbb06c1e2c0417843f2
j31337c <97733243+j31337c@users.noreply.github.com>
2023-11-18 20:33:30 +0000

    Update scratch.js

--- scratch/scratch.js
+++ scratch/scratch.js
@@ -12,7 +12,7 @@
       brush        = new Image();
       
   // base64 Workaround because Same-Origin-Policy
-  image.src = 'JPs7JDGg_400x400.jpg' width="300" height="300"
+  image.src = 'JPs7JDGg_400x400.jpg'
     image.onload = function() {
     ctx.drawImage(image, 0, 0);
     // Show the form when Image is loaded.
