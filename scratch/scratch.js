087c364b9789796aad2eb58a2075c8cbcff2d6cb
j31337c <97733243+j31337c@users.noreply.github.com>
2023-12-04 03:55:54 +0000

    Update scratch.js

--- scratch/scratch.js
+++ scratch/scratch.js
@@ -12,7 +12,7 @@
       brush        = new Image();
       
   // base64 Workaround because Same-Origin-Policy
-  image.src = 'https//astarter.app/scratch/img/bird-scratchcard.png'
+  image.src = 'bird-scratchcard.png'
     image.onload = function() {
     ctx.drawImage(image, 0, 0);
     // Show the form when Image is loaded.
