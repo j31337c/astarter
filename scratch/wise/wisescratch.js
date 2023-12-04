4d28d7b40fde10beac6494b8c329c004b57a79ca
j31337c <97733243+j31337c@users.noreply.github.com>
2023-12-04 00:03:30 +0000

    Update wisescratch.js

--- scratch/wise/wisescratch.js
+++ scratch/wise/wisescratch.js
@@ -12,7 +12,7 @@
       brush        = new Image();
       
   // base64 Workaround because Same-Origin-Policy
-  image.src = '../img/wise-scratchcard.png'
+  image.src = 'wise-scratchcard.png'
     image.onload = function() {
     ctx.drawImage(image, 0, 0);
     // Show the form when Image is loaded.
