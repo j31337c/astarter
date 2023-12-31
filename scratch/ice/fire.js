cda0ef084d6c744e2a154168bb9fc3f3fa04a6c6
j31337c <97733243+j31337c@users.noreply.github.com>
2023-12-31 19:10:41 +0000

    Update fire.js

--- scratch/ice/fire.js
+++ scratch/ice/fire.js
@@ -1,123 +1,106 @@
-window.addEventListener("resize", resizeCanvas, false);
-        window.addEventListener("DOMContentLoaded", onLoad, false);
-        
-        window.requestAnimationFrame = 
-            window.requestAnimationFrame       || 
-            window.webkitRequestAnimationFrame || 
-            window.mozRequestAnimationFrame    || 
-            window.oRequestAnimationFrame      || 
-            window.msRequestAnimationFrame     || 
-            function (callback) {
-                window.setTimeout(callback, 1000/60);
-            };
-        
-        var canvas, ctx, w, h, particles = [], probability = 0.04,
-            xPoint, yPoint;
-        
-        
-        
-        
-        
-        function onLoad() {
-            canvas = document.getElementById("canvas");
-            ctx = canvas.getContext("2d");
-            resizeCanvas();
-            
-            window.requestAnimationFrame(updateWorld);
-        } 
-        
-        function resizeCanvas() {
-            if (!!canvas) {
-                w = canvas.width = window.innerWidth;
-                h = canvas.height = window.innerHeight;
+import Fireworks from "https://esm.sh/@fireworks-js/react";
+import type { FireworksHandlers } from "https://esm.sh/@fireworks-js/react";
+import ReactDOM from "https://esm.sh/react-dom@18.2.0";
+import React, {
+  useRef,
+  useState,
+  FC,
+  RefObject,
+  Dispatch,
+  SetStateAction
+} from "https://esm.sh/react@18.2.0";
+
+interface ButtonProps {
+  fireworkRef: RefObject<HTMLDivElement>;
+}
+
+const App = () => {
+  const ref = useRef<FireworksHandlers>(null);
+
+  return (
+    <div class="container">
+      <Fireworks
+        ref={ref}
+        options={{
+          opacity: 0.5
+        }}
+        style={{
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          position: "fixed"
+        }}
+      />
+      <div class="btns">
+        <Follow fireworkRef={ref} />
+        <Fire fireworkRef={ref} />
+        <Reset fireworkRef={ref} />
+      </div>
+    </div>
+  );
+};
+
+const Reset: FC<ButtonProps> = ({ fireworkRef }) => {
+  const resetFireworks = () => {
+    fireworkRef.current.stop();
+    fireworkRef.current.start();
+  };
+  return (
+    <div class="btn" onClick={resetFireworks}>
+      Reset
+    </div>
+  );
+};
+
+const Fire: FC<ButtonProps> = ({ fireworkRef }) => {
+  const [fireCount, setFireCount] = useState(1);
+  return (
+    <div class="btn fire-btn">
+      <div
+        onClick={() => {
+          fireworkRef.current.launch(fireCount);
+        }}
+      >
+        Fire
+      </div>
+      <input
+        min={1}
+        max={15}
+        type="number"
+        class="fire-count"
+        value={fireCount}
+        onChange={(e: any) => setFireCount(e.target.value)}
+      />
+    </div>
+  );
+};
+
+const Follow: FC<ButtonProps> = ({ fireworkRef }) => {
+  const [follow, setFollow] = useState(false);
+  return (
+    <div
+      onClick={() => {
+        setFollow(!follow);
+        if (fireworkRef.current.currentOptions.mouse.move) {
+          fireworkRef.current.updateOptions({
+            mouse: {
+              move: false
             }
-        } 
-        
-        function updateWorld() {
-            update();
-            paint();
-            window.requestAnimationFrame(updateWorld);
-        } 
-        
-        function update() {
-            if (particles.length < 500 && Math.random() < probability) {
-                createFirework();
+          });
+        } else {
+          fireworkRef.current.updateOptions({
+            mouse: {
+              move: true
             }
-            var alive = [];
-            for (var i=0; i<particles.length; i++) {
-                if (particles[i].move()) {
-                    alive.push(particles[i]);
-                }
-            }
-            particles = alive;
-        } 
-        
-        function paint() {
-            ctx.globalCompositeOperation = 'source-over';
-            ctx.fillStyle = "rgba(0,0,0,0.2)";
-            ctx.fillRect(0, 0, w, h);
-            ctx.globalCompositeOperation = 'lighter';
-            for (var i=0; i<particles.length; i++) {
-                particles[i].draw(ctx);
-            }
-        } 
-        
-        function createFirework() {
-            xPoint = Math.random()*(w-200)+100;
-            yPoint = Math.random()*(h-200)+100;
-            var nFire = Math.random()*50+100;
-            var c = "rgb("+(~~(Math.random()*200+55))+","
-                 +(~~(Math.random()*200+55))+","+(~~(Math.random()*200+55))+")";
-            for (var i=0; i<nFire; i++) {
-                var particle = new Particle();
-                particle.color = c;
-                var vy = Math.sqrt(25-particle.vx*particle.vx);
-                if (Math.abs(particle.vy) > vy) {
-                    particle.vy = particle.vy>0 ? vy: -vy;
-                }
-                particles.push(particle);
-            }
-        } 
-        
-        function Particle() {
-            this.w = this.h = Math.random()*4+1;
-            
-            this.x = xPoint-this.w/2;
-            this.y = yPoint-this.h/2;
-            
-            this.vx = (Math.random()-0.5)*10;
-            this.vy = (Math.random()-0.5)*10;
-            
-            this.alpha = Math.random()*.5+.5;
-            
-            this.color;
-        } 
-        
-        Particle.prototype = {
-            gravity: 0.05,
-            move: function () {
-                this.x += this.vx;
-                this.vy += this.gravity;
-                this.y += this.vy;
-                this.alpha -= 0.01;
-                if (this.x <= -this.w || this.x >= screen.width ||
-                    this.y >= screen.height ||
-                    this.alpha <= 0) {
-                        return false;
-                }
-                return true;
-            },
-            draw: function (c) {
-                c.save();
-                c.beginPath();
-                
-                c.translate(this.x+this.w/2, this.y+this.h/2);
-                c.arc(0, 0, this.w, 0, Math.PI*2);
-                c.fillStyle = this.color;
-                c.globalAlpha = this.alpha;
-                
-                c.closePath();
-                c.fill();
-                c.restore();
-            }
-        } 
+          });
+        }
+      }}
+      class="btn"
+    >
+      {follow ? "Un-Follow" : "Follow"}
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
